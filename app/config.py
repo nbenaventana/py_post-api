43b8b51affbb0f2f9b4ea5e5830f892cb388e6ad
Nelson Benaventana <nbenaventana@gmail.com>
2024-11-06 20:09:42 -0300

    fix: db host

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ class Config:
     POSTGRES_DB = 'post_7kgu'
     POSTGRES_USER = 'root'
     POSTGRES_PASSWORD = 'zAkOJ4xxkqOP4NYuqAdHSAHIWJj2ULcC'
-    POSTGRES_HOST = 'dpg-cs7g54ij1k6c73fg5p20-a.oregon-postgres.render.com'
+    POSTGRES_HOST = 'dpg-cs7g54ij1k6c73fg5p20-a'
     POSTGRES_PORT = 5432
 
     DEBUG = True
